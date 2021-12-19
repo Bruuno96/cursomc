1a238076033b2966898e233e12d00b2e9e4f588f
Bruuno96 <brunotkd96@gmail.com>
2021-12-19 19:35:57 -0300

    feat: banco de dados mariadb e criação automatica da base de dados

--- src/main/java/com/brunomartin/cursomc/domain/Categoria.java
+++ src/main/java/com/brunomartin/cursomc/domain/Categoria.java
@@ -3,10 +3,16 @@ package com.brunomartin.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.*;
+
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 		
