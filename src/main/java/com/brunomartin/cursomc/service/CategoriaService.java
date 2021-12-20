7e4d22874c81b36e463dedaf79c171b05ebb2c5e
Bruuno96 <brunotkd96@gmail.com>
2021-12-19 21:12:42 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/brunomartin/cursomc/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.brunomartin.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunomartin.cursomc.domain.Categoria;
+import com.brunomartin.cursomc.repository.CategoriaRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No object with this ID"));
+	}
+}
