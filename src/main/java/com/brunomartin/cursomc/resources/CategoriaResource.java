f6d2df79ad31bcd6d350cdfa0a3a9fcedd01a182
Bruuno96 <brunotkd96@gmail.com>
2022-01-03 18:04:05 -0300

    Categorias DTO

--- src/main/java/com/brunomartin/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brunomartin/cursomc/resources/CategoriaResource.java
@@ -1,20 +1,24 @@
 package com.brunomartin.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.brunomartin.cursomc.domain.Categoria;
+import com.brunomartin.cursomc.dto.CategoriaDTO;
 import com.brunomartin.cursomc.service.CategoriaService;
 import com.brunomartin.cursomc.service.ProdutoService;
 
@@ -34,7 +38,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(c);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj, UriComponentsBuilder ucb){
 		obj = repository.create(obj);
 		URI uri = ucb.path("/categorias/{id}").buildAndExpand(obj.getId()).toUri();
@@ -51,6 +55,15 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		repository.delete(id);		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categoria = repository.findAll();	
+		
+		// Convertendo os elementos da lista de Categoria acima para CategoriaDTO. 
+		List<CategoriaDTO> listDto = categoria.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/brunomartin/cursomc/service/CategoriaService.java
+++ src/main/java/com/brunomartin/cursomc/service/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.brunomartin.cursomc.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -38,5 +40,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 }
