52710d506ee627fb96fdcb5c96756b7cc0a379fd
Bruuno96 <brunotkd96@gmail.com>
2022-02-14 17:39:03 -0300

    PUT validacao email repetido

--- src/main/java/com/brunomartin/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/brunomartin/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.brunomartin.cursomc.domain.Cliente;
+import com.brunomartin.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
